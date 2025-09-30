a760f142067da51519374e9de026b45dd486f3ed
sulthon abdul hakim <151292159+slabkim@users.noreply.github.com>
2025-09-30 19:01:05 +0700

    Menambahkan Login dan register

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'core/constants.dart';
 import 'data/repositories/nandogami_repository.dart';
 import 'data/services/api_service.dart';
 import 'state/item_provider.dart';
-// import 'ui/screens/home_screen.dart';
+import 'ui/screens/login_register_screen.dart';
 import 'ui/screens/main_screen.dart';
 
 class NandogamiApp extends StatelessWidget {
@@ -29,8 +30,32 @@ class NandogamiApp extends StatelessWidget {
           ),
           scaffoldBackgroundColor: AppColors.black,
         ),
-        home: const MainScreen(),
+        home: const _AuthGate(),
       ),
+    );
+  }
+}
+
+class _AuthGate extends StatelessWidget {
+  const _AuthGate();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        if (snapshot.hasData) {
+          return const MainScreen();
+        }
+
+        return const LoginRegisterScreen();
+      },
     );
   }
 }
