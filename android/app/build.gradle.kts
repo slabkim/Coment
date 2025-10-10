3ed860a31802f0319d6b25f5df75c247c0954195
sulthon abdul hakim <151292159+slabkim@users.noreply.github.com>
2025-10-10 19:50:27 +0700

    Update Android app ID to com.example.nandogami for Firebase compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.coment"
+    namespace = "com.example.nandogami"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.coment"
+        applicationId = "com.example.nandogami"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
